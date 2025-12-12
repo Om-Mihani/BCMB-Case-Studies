22d3d9d4af7e054b913db0471229663af7f19437
Vyshnavi Arunkumar <vyshnavi2962001@gmail.com>
2025-12-12 11:31:10 +0100

    Update task1_main.m

--- task1_main.m
+++ task1_main.m
@@ -7,14 +7,14 @@ tf = 10;
 y0 = 3;       % R1(0)
 N = 40;
 
-% --- Euler method ---
+% Euler method 
 [tE, yE] = euler_ode_solv(@model1, t0, tf, y0, N, p);
 
-% --- MATLAB ode45 ---
+% MATLAB ode45
 f = @(t,y) model1(t,y,p);
 [tO, yO] = ode45(f, [t0 tf], y0);
 
-% --- Plot ---
+% Plot
 figure;
 plot(tE, yE, 'o-', 'DisplayName','Euler'); hold on;
 plot(tO, yO, 'LineWidth',2, 'DisplayName','ode45');
@@ -22,3 +22,4 @@ xlabel('Time');
 ylabel('R1');
 legend;
 title('Task 1: Euler vs ode45');
+
