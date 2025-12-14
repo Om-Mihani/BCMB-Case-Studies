ce50cbb2850435be03e9fcf9869a4515fea24d83
Deeparamki <deeparamakrishnansubathra@gmail.com>
2025-12-14 11:54:26 +0100

    Update input_u.m

--- TASK 2/input_u.m
+++ TASK 2/input_u.m
@@ -1,9 +1,8 @@
 function u = input_u(t, p)
-    % Constant input
-    if ~isfield(p, 'freq')
+ if ~isfield(p, 'freq')
         u = 1;
     else
-        % Sinusoidal input u = sin(freq * t) + 1
+        % Sinusoidal input
         u = sin(p.freq * t) + 1;
     end
 end
